15b4d85b88c9cf0da6034561e2e2442170c2ada4
GiveMe-A-Name <a455300764b@hotmail.com>
2022-04-13 14:47:32 +0800

    feat(global): add global result type

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod editor;
 mod terminal;
 use editor::Editor;
 use terminal::Terminal;
+
+pub type Error = std::io::Error;
+pub type Result<T> = std::result::Result<T, Error>;
+
 fn main() {
     Editor::default().run();
 }
