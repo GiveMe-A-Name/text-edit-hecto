0cbcc589db7afdf7c7bdbbf41f3534841326f6f6
GiveMe-A-Name <a455300764b@hotmail.com>
2022-05-01 08:06:17 +0800

    refactor(row): extract new row module

--- src/document.rs
+++ src/document.rs
@@ -1,43 +1,6 @@
-use unicode_segmentation::UnicodeSegmentation;
-
 use crate::Result;
-use std::cmp;
+use crate::Row;
 use std::fs;
-pub struct Row {
-    content: String,
-    len: usize,
-}
-
-impl From<&str> for Row {
-    fn from(slice: &str) -> Self {
-        let content = slice.to_string();
-        let len = content[..].graphemes(true).count();
-        Row { content, len }
-    }
-}
-
-impl Row {
-    /// render a document's row into terminal
-    pub fn render(&self, start: usize, end: usize) -> String {
-        let end = cmp::min(end, self.content.len());
-        let start = cmp::min(start, end);
-        self.content[..]
-            .graphemes(true)
-            .skip(start)
-            .take(end - start)
-            .map(|grapheme| if grapheme == "\t" { " " } else { grapheme })
-            .collect::<String>()
-    }
-
-    pub fn len(&self) -> usize {
-        self.len
-    }
-
-    #[allow(dead_code)]
-    pub fn is_empty(&self) -> bool {
-        self.content.is_empty()
-    }
-}
 
 #[derive(Default)]
 pub struct Document {
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ extern crate clap;
 mod args;
 mod document;
 mod editor;
+mod row;
 mod terminal;
 
 use args::Args;
 use document::Document;
-use document::Row;
 use editor::Editor;
 use editor::Position;
+use row::Row;
 use terminal::Terminal;
 
 pub type Error = std::io::Error;
